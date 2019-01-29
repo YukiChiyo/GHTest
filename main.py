48a95b9bea488c06788e2cd67cb22f104932f61e
YukiChiyo <yuki.chiyojima@quest.co.jp>
2019-01-29 15:46:13 +0900

    test

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 print("文字列の長さを指定してください。")
 
-length = input(">> ")
+length = int(input(">> "))
 
 print("表示する記号を指定してください。")
 
 kigo = input(">> ")
 
-print(length * kigo)
+print(kigo * length)
 
 print("何かキーを押してください......")
 
